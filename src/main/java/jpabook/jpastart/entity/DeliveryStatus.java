553434be7c31628dac2085f4f1bb80be7e1b558b
pok3mon025 <pok3mon025@gmail.com>
2022-02-21 16:42:00 +0900

    다양한 연관관계 매핑

---
+++ src/main/java/jpabook/jpastart/entity/DeliveryStatus.java
@@ -0,0 +1,6 @@
+package jpabook.jpastart.entity;
+
+public enum DeliveryStatus {
+    READY,  //준비
+    COMP    //배송
+}
--- src/main/java/jpabook/jpastart/entity/Order.java
+++ src/main/java/jpabook/jpastart/entity/Order.java
@@ -20,6 +20,10 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date orderDate;
 
@@ -39,6 +43,11 @@ public class Order {
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
     }
 
     public Long getId() {
